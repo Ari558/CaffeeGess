f5a735dc775406f09d57b00a15082b6f108a0c76
ErickD70 <78987375+ErickD70@users.noreply.github.com>
2022-05-11 23:09:58 -0600

    update. tbl_tipoproduct

--- Base de Datos/GessCoffe(1).sql
+++ Base de Datos/GessCoffe(1).sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: programacion3
+-- Host: 127.0.0.1    Database: caffegesse
 -- ------------------------------------------------------
 -- Server version	8.0.28
 
@@ -14,30 +14,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `tbl_bebida`
---
-
-DROP TABLE IF EXISTS `tbl_bebida`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tbl_bebida` (
-  `idtbl_bebida` int NOT NULL AUTO_INCREMENT,
-  `Nombre` varchar(45) NOT NULL,
-  `Precio` float NOT NULL,
-  PRIMARY KEY (`idtbl_bebida`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tbl_bebida`
---
-
-LOCK TABLES `tbl_bebida` WRITE;
-/*!40000 ALTER TABLE `tbl_bebida` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tbl_bebida` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `tbl_carrito`
@@ -62,31 +38,6 @@ CREATE TABLE `tbl_carrito` (
 LOCK TABLES `tbl_carrito` WRITE;
 /*!40000 ALTER TABLE `tbl_carrito` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tbl_carrito` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `tbl_comida`
---
-
-DROP TABLE IF EXISTS `tbl_comida`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tbl_comida` (
-  `idtbl_comida` int NOT NULL AUTO_INCREMENT,
-  `Nombre` varchar(45) NOT NULL,
-  `Precio` float NOT NULL,
-  PRIMARY KEY (`idtbl_comida`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tbl_comida`
---
-
-LOCK TABLES `tbl_comida` WRITE;
-/*!40000 ALTER TABLE `tbl_comida` DISABLE KEYS */;
-INSERT INTO `tbl_comida` VALUES (1,'Panini',5),(2,'Sub',4.75);
-/*!40000 ALTER TABLE `tbl_comida` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -217,31 +168,6 @@ LOCK TABLES `tbl_orden` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `tbl_postres`
---
-
-DROP TABLE IF EXISTS `tbl_postres`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tbl_postres` (
-  `idtbl_postres` int NOT NULL AUTO_INCREMENT,
-  `Nombre` varchar(45) NOT NULL,
-  `Precio` float NOT NULL,
-  PRIMARY KEY (`idtbl_postres`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tbl_postres`
---
-
-LOCK TABLES `tbl_postres` WRITE;
-/*!40000 ALTER TABLE `tbl_postres` DISABLE KEYS */;
-INSERT INTO `tbl_postres` VALUES (1,'Chescake',2.75);
-/*!40000 ALTER TABLE `tbl_postres` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `tbl_producto`
 --
 
@@ -292,6 +218,31 @@ LOCK TABLES `tbl_registro` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `tbl_tipoproduct`
+--
+
+DROP TABLE IF EXISTS `tbl_tipoproduct`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_tipoproduct` (
+  `idtbl_tipoproduct` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(45) NOT NULL,
+  `precio` float NOT NULL,
+  `tipoproduct` enum('Comidas','Bebidas','Postres') NOT NULL,
+  PRIMARY KEY (`idtbl_tipoproduct`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_tipoproduct`
+--
+
+LOCK TABLES `tbl_tipoproduct` WRITE;
+/*!40000 ALTER TABLE `tbl_tipoproduct` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_tipoproduct` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `tbl_usuario`
 --
 
@@ -314,67 +265,6 @@ LOCK TABLES `tbl_usuario` WRITE;
 /*!40000 ALTER TABLE `tbl_usuario` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tbl_usuario` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping routines for database 'programacion3'
---
-/*!50003 DROP PROCEDURE IF EXISTS `SP_I_BEBIDA` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_I_BEBIDA`(PNOmbre varchar(50), PPrecio float)
-BEGIN
-insert into tbl_bebida(Nombre,Precio) values (PNombre,PPrecio);
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `SP_I_COMIDA` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_I_COMIDA`(PNombre varchar(50),PPrecio float)
-BEGIN
-insert into tbl_comida(Nombre,Precio) values (PNombre,PPrecio);
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `SP_I_POSTRES` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_I_POSTRES`(PNombre varchar(50), PPrecio float)
-BEGIN
-insert into tbl_postres (Nombre,Precio) values (PNombre,PPrecio);
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -385,4 +275,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-22  4:43:09
+-- Dump completed on 2022-05-11 23:05:00
